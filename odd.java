6dcabad1368ef090f55e234dff2d80b8b1e925ae
shanmugakesavan1996 <43901472+shanmugakesavan1996@users.noreply.github.com>
2018-10-06 14:26:11 +0530

    Update odd.java

--- odd.java
+++ odd.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
-
 class odd
 {
   public static void main(String args[])
   {
      int x;
-     System.out.println("Enter an integer to check if it is odd or even");
      Scanner in = new Scanner(System.in);
      x = in.nextInt();
     
