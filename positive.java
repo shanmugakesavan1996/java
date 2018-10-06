c9005c3a594c1c109ee3874fe9e864e164e15452
shanmugakesavan1996 <43901472+shanmugakesavan1996@users.noreply.github.com>
2018-10-06 14:26:55 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -5,7 +5,6 @@ class positive
  {
    int num;
    Scanner sc=new Scanner(System.in);
-   System.out.print("Enter any integer number: ");
    num=sc.nextInt();
    if(num>0)
      System.out.println(num + " is POSITIVE NUMBER.");
